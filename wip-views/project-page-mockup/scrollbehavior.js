fb67e10ca2ee9998e45640123ad210d654f7d403
Joshua Pham <joshua.pham@gmail.com>
2015-05-15 01:36:34 -0700

    added waypoints.js functionality but lost up-scrolling behavior. less memory fatiguing though.

--- wip-views/project-page-mockup/scrollbehavior.js
+++ wip-views/project-page-mockup/scrollbehavior.js
@@ -2,6 +2,52 @@ $(document).ready(function() {
 
 var landingHeight = $('.landing').innerHeight();
 
+var panes = ['demo', 'controls', 'downloads']; // ID's
+var activeNav;
+panes.forEach(function(idsuffix) {
+
+  $('section#' + idsuffix).waypoint(function(dir) {
+    var activeNav = $(this);
+    if (dir === 'down') {
+      activeNav = $(this).next();
+    }
+    var sectionId = activeNav.attr('id');
+    $('.header>.nav').removeClass('current-tab');
+    $('.header>.nav#' + idsuffix ).addClass('current-tab');
+  });
+
+ /* $('#' + idsuffix).waypoint({
+    handler: function() {
+      console.log('passed offsetted', idsuffix);
+      if ($(activeNav))
+        $(activeNav).removeClass('current-tab');
+      $('.header>.nav#tab-' + idsuffix).addClass('current-tab');
+      activeNav = $('.header>.nav#tab-' + idsuffix);
+    },
+    offset: function() {
+      return -$('#' + idsuffix).outerHeight();
+    }
+  })
+*/
+/*  var topWP = new Waypoint({
+    element: document.getElementById(idsuffix),
+    handler: function() {
+      $('.header>.nav#tab-' + idsuffix).addClass('current-tab');
+
+    }
+  });
+  var bottomWP = new Waypoint({
+    element: document.getElementById(idsuffix),
+    handler: function() {
+      $('.header>.nav#tab-' + idsuffix).removeClass('current-tab');
+      console.log(idsuffix, 'removed');
+    },
+    offset: function() {
+      return -document.getElementById(idsuffix).clientHeight;
+    }
+  });*/
+});
+
 $(document).scroll(function() {
 
     var scrollpos = $(this).scrollTop();
@@ -11,30 +57,5 @@ $(document).scroll(function() {
     } else
         $('.header-cont').removeClass('enter-content');
 
-    // to highlight panel names
-    /*for (var i = 0; i < $('.header>.nav').length; i++) {
-        if (scrollpos >= landingHeight * (i + 1) && scrollpos < landingHeight * (i + 2))
-            $('.header>.nav#tab-' + i).addClass('current-tab');
-        else $('.header>.nav#tab-' + i).removeClass('current-tab');
-    }*/
-
-    var panes = ['demo', 'controls', 'downloads'] // ID's
-    panes.forEach(function(idsuffix) {
-        var el = $('#' + idsuffix);
-        if (scrollpos >= $(el).offset().top && scrollpos <  $(el).offset().top + $(el).outerHeight()) {
-            $('.header>.nav#tab-' + idsuffix).addClass('current-tab');
-        }
-        else $('.header>.nav#tab-' + idsuffix).removeClass('current-tab');
-    })
-
-    if (scrollpos >= landingHeight && scrollpos < landingHeight * 2)
-        $('section .info').addClass('rigid-pane');
-    else
-        $('section .info').removeClass('rigid-pane');
-
-    // TODO: description area will probably be variable in content size. above for
-    // loop to calculate scroll positions will not be maintainable for those.
-
-})
-
+});
 });
