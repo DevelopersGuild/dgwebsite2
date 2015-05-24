5c142d0fe26665cfe82e1a9b5ab908b5e600b63f
Christopher Banh <chrisngobanh@gmail.com>
2015-05-24 01:51:15 -0700

    Changed MailChimp Url Added favicon for splash page Removed no longer needed instances where Express was serving static files from the /node_modules directory

--- server.js
+++ server.js
@@ -20,16 +20,13 @@ var server = app.listen(Config.SERVER_PORT, Config.SERVER_ADDRESS, function () {
 var io = require('socket.io')(server);
 
 app.use(express.static('public'));
-app.use(express.static('node_modules/animate.css'));
-app.use(express.static('node_modules/bootstrap/dist'));
-app.use(express.static('node_modules/jquery/dist'));
 
 var routerIndex = require('./routes');
 
 // Use the routers
 app.use(routerIndex);
 
-require('./events/index')(io);
+require('./events')(io);
 
 // Handle 404 Error
 app.use(function(req, res) {
