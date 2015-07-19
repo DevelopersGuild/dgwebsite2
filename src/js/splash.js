aa2b177ee057fc433b00b81a7f9de55537662376
Paul Merrill <pdm@pdm.me>
2015-07-18 18:50:38 -0700

    splash: remove go-to-top button
    Our page is so small it isn't very useful.
    Also, scrolling is not hard.

--- src/js/splash.js
+++ src/js/splash.js
@@ -21,43 +21,9 @@ $(window).load(function() {
 */
 });
 
-//go to top button function
-$("#gotop").mouseenter(function(){
-   $("#gotop").fadeTo("fast", 0.8);
-});
-$("#gotop").mouseleave(function(){
-   $("#gotop").fadeTo("fast", 0.3);
-});
-
-$(window).scroll(function() {
-  if (window.iOS) {
-    return;
-  }
-  var windowHeight = window.innerHeight;
-  if ($(this).scrollTop() > windowHeight){
-    $('#gotop').fadeIn("fast");
-  } else {
-    $('#gotop').stop().fadeOut("fast");
-  }
-});
-
-var windowHeight = window.innerHeight;
-if ($(this).scrollTop() > windowHeight) {
-  $('#gotop').show();
-}
-
-$("#gotop").click(function(){
-    $("html, body").animate({ scrollTop: 0}, 1000);
-});
-
 // iOS detection from http://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 window.iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 if (window.iOS) {
-    // The go-to-top button looks goofy on iOS with our current CSS.
-    $("#gotop").hide();
-
     // iOS doesn't support "background-attachment: fixed," and, in fact, does something weird, instead.
     $(".fixed-background").removeClass("fixed-background");
 }
-
-
