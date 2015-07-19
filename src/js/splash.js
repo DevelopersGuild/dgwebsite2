ba11ff053fc5bf67c76a2dbb69cdb5c5788c9301
Paul Merrill <pdm@pdm.me>
2015-07-18 21:35:39 -0700

    splash: comment out jQuery include: not used

--- src/js/splash.js
+++ src/js/splash.js
@@ -2,11 +2,11 @@
 
 'use strict';
 
+/*
 window.$ = window.jQuery = require('jquery');
 require('./jqModal.min');
 
 $(window).load(function() {
-    /*
     $('#modal').jqm({
       trigger: 'button#joinbutton'
     });
@@ -18,12 +18,14 @@ $(window).load(function() {
     $('#modal a').click(function() {
       $('#modal').jqmHide();
     });
-*/
 });
+*/
 
-// iOS detection from http://stackoverflow.com/questions/9038625/detect-if-device-is-ios
 window.iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 if (window.iOS) {
-    // iOS doesn't support "background-attachment: fixed," and, in fact, does something weird, instead.
-    $(".fixed-background").removeClass("fixed-background");
+    // iOS doesn't support "background-attachment: fixed", and, in fact, does something weird, instead.
+    //$('.fixed-background').removeClass('fixed-background');
+    var backgroundElements = document.getElementsByClassName('fixed-background');
+    while (backgroundElements.length)
+        backgroundElements[0].classList.remove('fixed-background');
 }
